78acec312598980ce70c8c93e5a27e0381840d26
RafaelM <dark-dreamn@hotmail.com>
2026-06-25 21:18:12 -0300

    feat: comunicação bem sucedida com a ia llama3

--- src/main/java/diolab/budgeting/service/BudgetingService.java
+++ src/main/java/diolab/budgeting/service/BudgetingService.java
@@ -5,32 +5,22 @@ import diolab.budgeting.repository.TransactionRepository;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class BudgetingService {
-    private final TransactionRepository repository;
-    private final ChatClient chatClient;
+    private final ChatModel chatModel;
 
-    public BudgetingService(TransactionRepository repository, ChatClient.Builder chatClientBuider) {
-        this.repository = repository;
-        this.chatClient = chatClientBuider.build();
+    @Autowired
+    public BudgetingService(ChatModel chatModel) {
+        this.chatModel = chatModel;
     }
 
-    @Tool(description = "Registrar uma nova transação financeira, seja uma despesa ou uma receita.")
-    public String registerTransaction(String description, double value, String type) {
-        Transaction transaction = new Transaction(description, BigDecimal.valueOf(value), type.toUpperCase());
-        repository.save(transaction);
-        return "Sucesso: Transação '" + description + "'de R$" + value + " gravada como " + type + " no banco de dados.";
-    }
-
-    @Tool(description = "Lista todas as transações financeiras gravadas até o momento.")
     public String processCommand(String textCommand) {
-        return this.chatClient.prompt()
-                .user(textCommand)
-                .tools(this)
-                .call()
-                .content();
+        return chatModel.call(textCommand);
     }
 }
